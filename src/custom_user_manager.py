1d3276fb09249402b6a5c8c3467206fed008f551
Diego da Silva Pereira <diego.pereira@tema.group>
2020-06-02 03:38:45 -0300

    Custom user manager

---
+++ src/custom_user_manager.py
@@ -0,0 +1,9 @@
+from flask_user import UserManager
+from flask_user.forms import LoginForm
+from .forms import *
+
+class CustomUserManager(UserManager):
+
+    def customize(self, app):
+        self.RegisterFormClass = CustomRegisterForm
+        self.LoginFormClass = LoginForm
