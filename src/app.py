e7d61734174890ef2a11947b46531e9fc9798417
Diego da Silva Pereira <diego.pereira@tema.group>
2020-06-01 21:39:27 -0300

    Make app executable

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+import os
+
 app = Flask(__name__,
             template_folder='templates',
             static_folder='static',
@@ -10,3 +12,7 @@ app = Flask(__name__,
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
