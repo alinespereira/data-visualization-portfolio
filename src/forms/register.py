67a8fde13228093d674815de6396136419a054ef
Diego da Silva Pereira <diego.pereira@tema.group>
2020-06-04 15:25:48 -0300

    I18n

--- src/forms/register.py
+++ src/forms/register.py
@@ -1,17 +1,21 @@
 from flask_user.forms import RegisterForm
 from wtforms import StringField, PasswordField, validators
+from flask_babel import lazy_gettext
 
 
 class CustomRegisterForm(RegisterForm):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField(
-        'Email Address', [validators.Length(min=6, max=35), validators.Email()])
+    username = StringField(lazy_gettext('Username'), [
+                           validators.Length(min=4, max=25)])
+    email = StringField(lazy_gettext('Email'), [
+                        validators.Length(min=6, max=35), validators.Email()])
 
-    first_name = StringField('First Name', [validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.DataRequired()])
+    first_name = StringField(lazy_gettext('First Name'), [
+                             validators.DataRequired()])
+    last_name = StringField(lazy_gettext('Last Name'), [
+                            validators.DataRequired()])
 
-    password = PasswordField('New Password', [
+    password = PasswordField(lazy_gettext('Password'), [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
-    confirm = PasswordField('Repeat Password')
+    confirm = PasswordField(lazy_gettext('Confirm your password'))
